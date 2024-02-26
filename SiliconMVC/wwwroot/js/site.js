45b86dd3b9ac567390f5c74579aa29ddf22ba319
daBard <50872455+daBard@users.noreply.github.com>
2024-02-26 16:29:14 +0100

    Add project files.

---
+++ SiliconMVC/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let domMenu = document.getElementById("menu")
+
+function MenuButton_Clicked() {
+    domMenu.classList.toggle("active")
+}
+
+
