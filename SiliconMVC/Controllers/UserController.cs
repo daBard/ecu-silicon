80a9bc24d2ebb34b9fc3673cce32a606e13a5ce9
daBard <bard.johan@gmail.com>
2024-03-01 15:26:28 +0100

    added sections for views Signin, Signup and AccountDetails added views for Signin and Signup added partials for all forms in Signin, Signup and AccountDetails

---
+++ SiliconMVC/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Signin()
+        {
+            ViewData["Title"] = "Login";
+
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            ViewData["Title"] = "New Account";
+
+            return View();
+        }
+    }
+}
--- SiliconMVC/wwwroot/js/site.js
+++ SiliconMVC/wwwroot/js/site.js
@@ -3,10 +3,29 @@
 
 // Write your JavaScript code.
 
+//// Mobile menu
 let domMenu = document.getElementById("menu")
 
 function MenuButton_Clicked() {
     domMenu.classList.toggle("active")
 }
 
+//// Light/Dark Toggle
+let domCheckbox = document.querySelector('input[name=lightdarkcheckbox]')
 
+// set initiial
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    domCheckbox.checked = true;
+} else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    domCheckbox.checked = false;
+}
+
+// switch theme if checkbox is engaged
+domCheckbox.addEventListener('change', (cb) => {
+    document.documentElement.setAttribute(
+        'data-theme',
+        cb.target.checked ? 'dark' : 'light'
+    );
+});
