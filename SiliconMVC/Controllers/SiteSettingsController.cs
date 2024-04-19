9a6d14af6f335ad214907a87262435749bf1eb66
daBard <50872455+daBard@users.noreply.github.com>
2024-04-19 13:08:21 +0200

    added cookie consent functionality

---
+++ SiliconMVC/Controllers/SiteSettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconMVC.Controllers
+{
+    public class SiteSettingsController : Controller
+    {
+        [HttpPost]
+        public IActionResult ConsentCookies()
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(60),
+                HttpOnly = true,
+                Secure = true
+            };
+
+            Response.Cookies.Append("CookieConsent", "true", option);
+            return Ok();
+        }
+    }
+}
